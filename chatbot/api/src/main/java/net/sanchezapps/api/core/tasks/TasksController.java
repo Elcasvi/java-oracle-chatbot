3fe64b6fb1044c3ac9833d5fc24ac325da454d08
Elcasvi <casvi.sanchez@gmail.com>
2024-04-13 20:32:55 -0600

    Reactive non-blocking apis in tasks-service are added

--- chatbot/api/src/main/java/net/sanchezapps/api/core/tasks/TasksController.java
+++ chatbot/api/src/main/java/net/sanchezapps/api/core/tasks/TasksController.java
@@ -2,29 +2,30 @@ package net.sanchezapps.api.core.tasks;
 
 
 import org.springframework.web.bind.annotation.*;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 
-import java.util.List;
 public interface TasksController {
     @GetMapping(value = "/tasks")
-    List<Task> getAll();
+    Flux<Task> getAll();
 
     @GetMapping(value = "/tasks/{taskId}")
-    Task getById(@PathVariable("taskId") Long taskId);
+    Mono<Task> getById(@PathVariable("taskId") Long taskId);
 
     @GetMapping(value = "/users/{userId}/tasks")
-    List<Task> getAllByUserId(@PathVariable("userId") Long userId);
+    Flux<Task> getAllByUserId(@PathVariable("userId") Long userId);
 
     @GetMapping(value = "/users/{userId}/tasks/filter/byPriority")
-    List<Task> getAllByUserIdAndPriority(@PathVariable("userId") Long userId, @RequestParam TaskPriority priority);
+    Flux<Task> getAllByUserIdAndPriority(@PathVariable("userId") Long userId, @RequestParam TaskPriority priority);
 
     @GetMapping(value = "/users/{userId}/tasks/filter/byState")
-    List<Task> getAllByUserIdAndState(@PathVariable("userId") Long userId, @RequestParam TaskState state);
+    Flux<Task> getAllByUserIdAndState(@PathVariable("userId") Long userId, @RequestParam TaskState state);
 
     @PostMapping(value = "/task")
-    void create(@RequestBody Task task);
+    Mono<Task> create(@RequestBody Task task);
 
     @PutMapping(value = "/task/{taskId}")
-    void update(@PathVariable("taskId") Long taskId, @RequestBody Task task);
+    Mono<Task> update(@PathVariable("taskId") Long taskId, @RequestBody Task task);
 
     @DeleteMapping(value = "/task/{taskId}")
     void delete(@PathVariable("taskId") Long taskId);
