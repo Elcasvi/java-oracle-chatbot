fbf9e1ed4ca54db67c313776f4617fa3771e66be
Elcasvi <casvi.sanchez@gmail.com>
2024-04-15 08:31:01 -0600

    React-dom-router added

--- chat-bot-react/src/App.js
+++ chat-bot-react/src/App.js
@@ -1,11 +1,22 @@
 import Login from './pages/Login';
 import './App.css';
+import {BrowserRouter as Router,Routes,Route}from "react-router-dom"
+import HomePage from "./pages/HomePage";
 
 function App() {
   return (
+      <Router>
+          <Routes>
+              <Route path="/" element={<Login/>}/>
+              <Route path="/homePage" element={<HomePage/>}/>
+          </Routes>
+      </Router>
+
+      /*
     <div className="App">
         <Login />
     </div>
+       */
   );
 }
 
