1a8380dc5268889800034427b6bc718f28216489
Elcasvi <casvi.sanchez@gmail.com>
2024-04-17 08:54:43 -0600

    Cors is added

--- chatbot/tasks-service/src/main/java/net/sanchezapps/tasksservice/services/TasksControllerImpl.java
+++ chatbot/tasks-service/src/main/java/net/sanchezapps/tasksservice/services/TasksControllerImpl.java
@@ -5,6 +5,7 @@ import net.sanchezapps.api.core.tasks.TaskPriority;
 import net.sanchezapps.api.core.tasks.TaskState;
 import net.sanchezapps.api.core.tasks.TasksController;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RestController;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
@@ -13,6 +14,7 @@ import java.io.IOException;
 
 
 @RestController
+@CrossOrigin("http://localhost:3000/")
 public class TasksControllerImpl implements TasksController {
 
     private final TasksService service;
--- chatbot/users-service/src/main/java/net/sanchezapps/usersservice/services/UsersControllerImpl.java
+++ chatbot/users-service/src/main/java/net/sanchezapps/usersservice/services/UsersControllerImpl.java
@@ -10,7 +10,7 @@ import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 @RestController
-@CrossOrigin("*")
+@CrossOrigin("http://localhost:3000/")
 public class UsersControllerImpl implements UsersController {
     private final UsersService service;
     @Autowired
