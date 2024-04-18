720c37e4afe6ec437840ed5fcc4692ea98c41ec6
Elcasvi <casvi.sanchez@gmail.com>
2024-04-18 09:29:22 -0600

    CORS updated

--- chatbot/tasks-service/src/main/java/net/sanchezapps/tasksservice/config/CorsConfig.java
+++ chatbot/tasks-service/src/main/java/net/sanchezapps/tasksservice/config/CorsConfig.java
@@ -1,3 +1,5 @@
+package net.sanchezapps.tasksservice.config;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
