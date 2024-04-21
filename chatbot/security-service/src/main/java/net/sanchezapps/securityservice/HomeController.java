c474fff15c84c2d04a9e0a4c8ca706227b9914fa
Elcasvi <casvi.sanchez@gmail.com>
2024-04-21 09:16:29 -0600

    k8s deployment succesful

---
+++ chatbot/security-service/src/main/java/net/sanchezapps/securityservice/HomeController.java
@@ -0,0 +1,12 @@
+package net.sanchezapps.securityservice;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+    @GetMapping("/")
+    public String helloWorld() {
+        return "Hello, World!";
+    }
+}
