4641a196e8d16142bdce7195ae961554a68ea9d6
Elcasvi <casvi.sanchez@gmail.com>
2024-04-27 11:40:38 -0600

    Constraints of DB tables updated

--- chatbot/users-service/src/main/java/net/sanchezapps/usersservice/persistence/UserEntity.java
+++ chatbot/users-service/src/main/java/net/sanchezapps/usersservice/persistence/UserEntity.java
@@ -1,6 +1,9 @@
 package net.sanchezapps.usersservice.persistence;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.*;
 import net.sanchezapps.api.core.users.Role;
 
@@ -16,8 +19,16 @@ public class UserEntity {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "id",unique = true,nullable = false)
     private Long id;
+    @NotBlank
+    @Size(max=255)
     private String name;
+    @NotBlank
+    @Email
+    @Column(unique = true)
     private String email;
+    @NotBlank
+    @Size(min = 4)
     private String password;
+    @Enumerated(EnumType.STRING)
     private Role role;
 }
