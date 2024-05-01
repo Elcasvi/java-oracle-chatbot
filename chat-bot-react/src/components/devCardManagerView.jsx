4dba3758aa453e504ddda20aa7500d481f221d1c
LilianaHg <A01640873@tec.mx>
2024-05-01 11:59:29 -0600

    Developer page

--- chat-bot-react/src/components/devCardManagerView.jsx
+++ chat-bot-react/src/components/devCardManagerView.jsx
@@ -1,15 +1,13 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+    import React from 'react';
+    import { Link } from 'react-router-dom';
 
 function DevCardManagerView({ user }) {
     return (
         <article className='dev-card-manager'>
             <header className="dev-card-manger-header">
-                <Link to={`/tasks/${user.id}`}>
-                    <img className="dev-card-manger-icon"
-                        alt="Dev Icon"
-                        src="https://unavatar.io/user"></img>
-                </Link>
+                <img className="dev-card-manger-icon"
+                    alt="Dev Icon"
+                    src="https://unavatar.io/user"></img>
                 <div className="dev-card-manager-name">
                     <strong>{user.name}</strong>
                     <span className="dev-card-manager-numTask">Numero de Task Asignadas: {user.tasks.length}</span>
@@ -17,12 +15,10 @@ function DevCardManagerView({ user }) {
             </header>
 
             <div className='dev-card-manager-button'>
-                <div>
-                    <Link to={`/tasks/${user.id}`} className="dev-card-manager-showMore">Ver Más</Link>
-                </div>
+                <Link to={`/tasks/${user.id}`} className="dev-card-manager-showMore">Ver Más</Link>
             </div>
         </article>
     );
 }
 
-export default DevCardManagerView;
+    export default DevCardManagerView;
