e9c0d1092a4509ce215210ddedbb2ea59b37c1a9
Elcasvi <casvi.sanchez@gmail.com>
2024-05-01 23:18:45 -0600

    oke endpoints updated

--- chat-bot-react/src/services/taskServices.js
+++ chat-bot-react/src/services/taskServices.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_BASE_URL = "http://140.84.170.209";
+const API_BASE_URL = "http://159.54.143.123";
 
 const api = axios.create({
     baseURL: API_BASE_URL,
--- chat-bot-react/src/services/userServices.js
+++ chat-bot-react/src/services/userServices.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_BASE_URL = "http://159.54.136.5";
+const API_BASE_URL = "http://159.54.133.251";
 
 const api = axios.create({
     baseURL: API_BASE_URL,
