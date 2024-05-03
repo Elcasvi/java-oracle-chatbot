5416c4493d7ae6ca0e97546af5f140c0c97623bc
unknown <A01662438@tec.mx>
2024-05-03 02:37:03 -0600

    Servicioes

--- chat-bot-react/src/services/taskServices.js
+++ chat-bot-react/src/services/taskServices.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
-//const API_BASE_URL = "https://oketasksservice.sanchezapps.net/";
-const API_BASE_URL = "http://127.0.0.1:62941";
+const API_BASE_URL = "https://oketasksservice.sanchezapps.net/";
+//const API_BASE_URL = "http://127.0.0.1:62941";
 
 const api = axios.create({
     baseURL: API_BASE_URL,
