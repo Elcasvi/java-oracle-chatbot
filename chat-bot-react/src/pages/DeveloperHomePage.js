88aa9c75a101dbcdf88a00fa272ac91eac1b0cc1
unknown <88795402+JavierP08@users.noreply.github.com>
2024-06-11 19:29:47 -0600

    Update a homePageDeveloper

--- chat-bot-react/src/pages/DeveloperHomePage.js
+++ chat-bot-react/src/pages/DeveloperHomePage.js
@@ -5,17 +5,18 @@ import AllTasks from '../components/AllTaskList';
 import OptionsBar from '../components/optionsBar.jsx';
 import LogoutButton from '../components/LogoutButton.jsx';
 import '../HomePage.css';
-import taskServices from '../services/taskServices.js';
 import userServices from '../services/userServices.js';
 
 export default function DeveloperHomePage({ userId }) {
     const [filteredTasks, setFilteredTasks] = useState(null);
+    const [ nombre, setNombre ] = useState(null);
 
     const getAllTask = async () => {
         const userService = new userServices();
         const response = await userService.getUserById(userId);
         if (response && response.data){
-            setFilteredTasks(response.data.tasks);
+            setFilteredTasks(response.data.tasks)
+            setNombre(response.data.name)
         } else {
             setFilteredTasks([]);
         }
@@ -27,14 +28,18 @@ export default function DeveloperHomePage({ userId }) {
         }
     },[userId])
 
+    const handleSuccess = () => {
+        getAllTask();
+    };
+
     const handleSelectOption = (option) => {
         if (option === 'Filter by Status') {
             const statusOrder = {
                 'TODO': 3,
-                'IN PROGRESS': 2,
+                'IN_PROGRESS': 2,
                 'DONE': 1
             };
-            const sortedTasks = Tasks.slice().sort((a, b) => {
+            const sortedTasks = filteredTasks.slice().sort((a, b) => {
                 return statusOrder[a.status] - statusOrder[b.status];
             });
             setFilteredTasks(sortedTasks);
@@ -44,7 +49,7 @@ export default function DeveloperHomePage({ userId }) {
                 'MEDIUM': 2,
                 'LOW': 1
             };
-            const sortedTasks = Tasks.slice().sort((a, b) => {
+            const sortedTasks = filteredTasks.slice().sort((a, b) => {
                 return priorityOrder[b.priority] - priorityOrder[a.priority];
             });
             setFilteredTasks(sortedTasks);
@@ -55,9 +60,9 @@ export default function DeveloperHomePage({ userId }) {
         <div className="dev-home-page-container">
             <LogoutButton></LogoutButton>
             <h1 style={{ fontFamily: 'Lato, sans-serif', color: 'black', fontSize: '30px', textAlign: 'left' }}>
-                    Welcome Back "El fuckin nombre aqui"
+                    Welcome Back {nombre}
             </h1>            
-            <OptionsBar onSelectOption={handleSelectOption} />
+            <OptionsBar onCreateSuccess={handleSuccess} onSelectOption={handleSelectOption} userId={userId} />
             {filteredTasks === null ? (
                 <p>Cargando tareas...</p>
             ):(
