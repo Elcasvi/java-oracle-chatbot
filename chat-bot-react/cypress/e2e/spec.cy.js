9bc0e61f005aadf9182de1e25d856a5998f04a3a
unknown <88795402+JavierP08@users.noreply.github.com>
2024-06-13 23:11:06 -0600

    Testing Manager

--- chat-bot-react/cypress/e2e/spec.cy.js
+++ chat-bot-react/cypress/e2e/spec.cy.js
@@ -1,5 +1,18 @@
-describe('template spec', () => {
-  it('passes', () => {
-    cy.visit('https://example.cypress.io')
-  })
-})
+describe('Login Test', () => {
+  it('logs into the website', () => {
+    cy.visit('https://chatbot.sanchezapps.net/');
+    cy.get('input[type="email"]').type('a01640873@tec.mx');
+    cy.get('input[type="password"]').type('password');
+    cy.contains('Iniciar Sesión').click();
+
+    cy.wait(3000); 
+    cy.contains('tr', 'Chat-Bot').last().within(() => {
+      cy.get('button').contains('View Members').click();
+    });
+
+    cy.wait(3000); 
+    cy.contains('tr', 'Erick Orozco').within(() => {
+      cy.get('svg').click();
+    });
+  });
+});
